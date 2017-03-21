3e2dcf20d5be3377544df15cf1c76589e37a817b
TuXiaokang <931085856@qq.com>
2017-03-21 16:41:48 +0800

    remove landmarks type

--- common.py
+++ common.py
@@ -22,7 +22,11 @@ class Face(object):
         self.y = 0
         self.w = 0
         self.h = 0
-        self.landmarks = [(0,0)] * 5
+    
 
 class _Image(Structure):
-    _fields_ = [('data', c_void_p),('width',c_int),('height',c_int),('channels',c_int)]
+    _fields_ = [
+        ('data', c_void_p),
+        ('width',c_int),
+        ('height',c_int),
+        ('channels',c_int)]
