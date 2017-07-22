fa16fd3c91cd2f2d198ffe7e917ca31e2e0f0660
TuXiaokang <931085856@qq.com>
2017-07-23 04:18:46 +0800

    fix setup.py error

--- setup.py
+++ setup.py
@@ -8,21 +8,21 @@ dylibs = [os.path.join(dylib_dir, x) for x in os.listdir(dylib_dir) if x.split('
 
 setup(
     name ='pyseeta',
-        
-    version = 0.3.0,
-    
+
+    version = 0.3,
+
     description = 'A simple Python interface for the SeetaFaceEngine',
- 
+
     author = 'Xiaokang Tu',
-    
+
     license = 'MIT',
-    
+
     classifiers = [
 
         'Development Status :: 5 - Production/Stable',
 
         'Intended Audience :: Developers',
-        
+
         'Operating System :: MacOS',
         'Operating System :: Microsoft :: Windows',
         'Operating System :: POSIX :: Linux',
@@ -30,16 +30,16 @@ setup(
         'Topic :: Software Development :: Build Tools',
         'Topic :: Software Development :: Debuggers',
         'Topic :: Software Development :: Embedded Systems',
-        
+
         'License :: OSI Approved :: MIT License',
 
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6'
     ],
-    
+
     keywords = 'pyseeta seetaface face-detection face-alignment face-identification',
-    
+
     packages = find_packages(),
 
     install_requires = ['numpy'],
